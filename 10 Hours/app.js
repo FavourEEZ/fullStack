6020e37a6b38af948c1786882eb3cd4afc0b0913
Favour Omoragbon <favouromoragbon2002@gmail.com>
2022-05-07 07:47:56 +0100

    Connect css, obtain packages

--- 10 Hours/app.js
+++ 10 Hours/app.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const app = express();
 const port = 10001;
+const path = require('path');
 
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 app.get("/", (req, res) => {
     res.render("home");
