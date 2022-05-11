70c48c0b115778fce67b80a65da8bc0e1cb13824
Favour Omoragbon <favouromoragbon2002@gmail.com>
2022-05-11 22:27:58 +0100

    Install chartjs and setup basic code -full integration needed

--- 10 Hours/public/javascript/home.js
+++ 10 Hours/public/javascript/home.js
@@ -1,13 +1,28 @@
 // let getTime = '<%time%>'
-
+const Chart = require('chart.js');
 const timeHeader = document.getElementById("time-header");
 const dateHeader = document.getElementById("date-header");
 setInterval( () => {
-    let time = new Date()//.toLocaleTimeString();
-    console.log(time);
-    console.log(time.toLocaleDateString())
-    dateHeader.innerHTML = time.toDateString()//date.toLocaleDateString()
+    let time = new Date()
+    // console.log(time);
+    // console.log(time.toLocaleDateString())
+    dateHeader.innerHTML = time.toDateString()
     timeHeader.innerText = time.toLocaleTimeString()
 }, 1000
 )
+
+const getChart = document.getElementById("chart").getContext("2");
+const chart = new Chart(getChart, {
+    type: 'line',
+    data: {
+        labels: ["Week-1", "Week-2", "Week-3", "Week-4", "Week-5"],
+        dataset: [{
+            label: "Week productivity",
+            data: [2, 4, 7, 1, 9],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1
+        }]
+    }
+})
    
