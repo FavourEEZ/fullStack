ff1cb4cec9feded009bd86b9a825c708e228d205
Favour Omoragbon <favouromoragbon2002@gmail.com>
2022-05-12 21:43:54 +0100

    Add post routing for login and signup page

--- 10 Hours/app.js
+++ 10 Hours/app.js
@@ -4,9 +4,11 @@ const port = 10000;
 const path = require('path');
 const chart = require("chart.js");
 
+const bodyParser = require("body-parser");
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'public')))
-
 
 app.get("/", (req, res) => {
     res.render("home", {time: "Time"});
@@ -20,13 +22,20 @@ app.get("/login", (req, res) =>{
     res.render("loginPage")
 })
 
-// app.post("/signup", async(req, res) =>{
-//     const {username, email, password } = req.body;
-
-// })
+app.post("/login", urlencodedParser, (req, res) =>{
+    console.log(req.body)
+    const {username, password } = req.body;
+    // res.send('welcome back, ' + req.body.username)
+})
 
 app.get("/signup", (req, res) => {
     res.render("signup")
+})
+
+app.post("/signup", urlencodedParser, (req, res) =>{
+    console.log(req.body)
+    const {username, email, password } = req.body;
+    // res.send('Signed Up!, ' + req.body.username)
 })
 
 app.listen(port, () =>{
